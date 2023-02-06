cc02add496514893b8a9fffdb103b754fb99fc7f
hoess-git <20182011@atn.ac.at>
2023-02-06 11:49:43 +0100

    Fix Snackbar

--- WASM/Program.cs
+++ WASM/Program.cs
@@ -8,18 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddMudServices(config =>
-{
-	config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
-
-	config.SnackbarConfiguration.PreventDuplicates = false;
-	config.SnackbarConfiguration.NewestOnTop = false;
-	config.SnackbarConfiguration.ShowCloseIcon = true;
-	config.SnackbarConfiguration.VisibleStateDuration = 10000;
-	config.SnackbarConfiguration.HideTransitionDuration = 500;
-	config.SnackbarConfiguration.ShowTransitionDuration = 500;
-	config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
-});
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
